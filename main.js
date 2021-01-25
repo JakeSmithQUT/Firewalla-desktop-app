998166ca174ce11f6ebf86725261bb259190ba9a
Jake Smith <jsmith@EEHA.Local>
2021-01-25 21:46:36 +1000

    Initial Commit of files for application

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.removeMenu()
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
